2e6aa358a5d67aecdb98f16f0a02d0a10321e28b
Daniel Taylor <DDT1-Old@wireless-10-104-126-49.umd.edu>
2017-03-21 15:40:39 -0400

    updated app and made ticker_plot.html

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+app.vars = {}
+
 @app.route('/')
 def main():
   return redirect('/index')
@@ -9,6 +11,12 @@ def main():
 @app.route('/index')
 def index():
   return render_template('index.html')
+
+@app.rout('/ticker_plot', methods=['POST'])
+def plot_result():
+	app.vars['stock_name'] = request.form['stock_name']
+	return render_template('ticker_plot.html', name=app.vars['stock_name'])
+
 
 if __name__ == '__main__':
   app.run(port=33507)
