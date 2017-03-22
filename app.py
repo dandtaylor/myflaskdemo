4fbd0f4aebfdbdb7d4f4dbc67e7a1f7fe482e068
Dan <dandtaylor@gmail.com>
2017-03-22 18:44:13 +0000

    added date and plot data options

--- app.py
+++ app.py
@@ -23,7 +23,8 @@ def index():
 @app.route('/ticker_plot', methods=['POST'])
 def plot_result():
 	app.vars['stock_name'] = request.form['stock_name']
-	payload = {'date.gte': '20170101', 'ticker': app.vars['stock_name'],
+	app.vars['plot_data'] = request.form['plot_data']
+	payload = {'date.gte': request.form['start_date'], 'ticker': app.vars['stock_name'],
                 'api_key': app.api_key}
 	r = requests.get('https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?',
                     params=payload)
@@ -37,14 +38,15 @@ def plot_result():
 	df = df.set_index(pd.DatetimeIndex(df['date']))
 	date = df.ix[0, 'date']
 	name = df.ix[0, 'ticker']
-	open_price = df.ix[0, 'open']
+	open_price = df.ix[0, app.vars['plot_data']]
 
-	p = figure(x_axis_type='datetime', title='Closing prices for %s' % app.vars['stock_name'].upper(), 
-				plot_width=500, plot_height=500)
+	TOOLS = ('hover,crosshair,pan,zoom_in,zoom_out,box_zoom,undo,redo,reset,tap,save')
+	p = figure(x_axis_type='datetime', title='%s for %s' % (app.vars['plot_data'], app.vars['stock_name'].upper()), 
+				plot_width=500, plot_height=500, tools=TOOLS)
 	p.grid.grid_line_alpha = 0.3
 	p.xaxis.axis_label = 'Date'
-	p.yaxis.axis_label = 'Closing Price'
-	p.line(np.array(df['date'], dtype=np.datetime64), df['close'], color='red')
+	p.yaxis.axis_label = app.vars['plot_data']
+	p.line(np.array(df['date'], dtype=np.datetime64), df[app.vars['plot_data']], color='blue')
 
 	script, div  = components(p)
 
